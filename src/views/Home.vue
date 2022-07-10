5f4d3322639f4ad8fd50b53e3856014c5b1b0ab5
Michael Gerges <engmichaelgerges@gmail.com>
2022-07-10 11:08:05 +0200

    add main header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="line-chart container">
-    <h1 class="text-center">ui task</h1>
+  <div class="home-page">
+    <Header />
   </div>
 </template>
 <script>
-// @ is an alias to /src
-import Vue from "vue";
-import vSelect from "vue-select";
-Vue.component("v-select", vSelect);
-import "vue-select/dist/vue-select.css";
-
-import "simple-scrollbar/simple-scrollbar.css";
+import Header from './../components/Header.vue'
 
 export default {
   name: "Home",
   components: {
+    Header
   },
   data() {
     return {
-      value: null,
-      dimensionSelected: "Product",
-      measureSelected: 'Cost'
     };
   },
   methods: {},
@@ -28,22 +20,5 @@ export default {
 };
 </script>
 <style lang="scss">
-.ss-scroll {
-  background-color: red;
-  opacity: 1;
-}
-
-// .multiselect__content-wrapper {
-//   height: 200px;
-// }
-
-.test .vs__dropdown-menu {
-  //ul
-  height: 200px;
-  overflow: hidden;
-}
-.line-chart {
-  padding: 40px 0;
-}
 
 </style>
